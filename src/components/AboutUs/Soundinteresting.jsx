43453d42c0f2e4528f440b3900c5d6ca1c6c390e
priyanshu Chaudhary <priyanshuchaudhary@priyanshus-MacBook-Pro.local>
2022-06-11 15:48:01 +0530

    fixed imports 4th file

--- src/components/AboutUs/Soundinteresting.jsx
+++ src/components/AboutUs/Soundinteresting.jsx
@@ -15,7 +15,7 @@ return (
     <div className='greydiv mb-5'>
         <h2>{props.heading}</h2>
         <p className='mt-3'>{props.para}</p>
-        <a className='btn btn-dark rounded-pill px-5 py-3 mt-3 joinus' onClick={handleClick} target={props.newpage === "true" ? "_blank" : null} rel={props.newpage === "true" ? "noreferrer" : null} href={props.link}>{props.button}</a>
+        {/* <a className='btn btn-dark rounded-pill px-5 py-3 mt-3 joinus' onClick={handleClick} target={props.newpage === "true" ? "_blank" : null} rel={props.newpage === "true" ? "noreferrer" : null} href={props.link}>{props.button}</a> */}
     </div>
 )
 }
